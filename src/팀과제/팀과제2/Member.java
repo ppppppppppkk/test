c691f553e6fec877f61a9a1209c58797aa5c41e0
total5637 <total5637@naver.com>
2024-01-15 18:09:58 +0900

    24-01-15 18:09 커밋

---
+++ src/팀과제/팀과제2/Member.java
@@ -0,0 +1,33 @@
+package 팀과제.팀과제2;
+
+public class Member {
+    static int count = 0;
+
+    String id;
+    String pw;
+    String name;
+    String rrn;
+    String phone;
+    int memberNum;
+
+    Member(String id, String pw, String name, String rrn, String phone){
+        this.id = id;
+        this.pw = pw;
+        this.name = name;
+        this.rrn = rrn;
+        this.phone = phone;
+        this.memberNum = count;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id='" + id + '\'' +
+                ", pw='" + pw + '\'' +
+                ", name='" + name + '\'' +
+                ", rrn='" + rrn + '\'' +
+                ", phone='" + phone + '\'' +
+                ", memberNum=" + memberNum +
+                '}';
+    }
+}
---
+++ src/팀과제/팀과제2/Service.java
@@ -0,0 +1,35 @@
+package 팀과제.팀과제2;
+
+public class Service {
+
+    // 필드
+    int memberNum;  // 고객고유 번호
+    String rating;  // 등급
+    double volume;  // 용량 10.0 , 20,0 30,0
+    String period; // 30일
+    int price;  // 5000 , 10000 ,15000
+
+    // 메소드
+
+    void basic(int memberNum){
+        this.memberNum = memberNum;
+        this.rating = "Basic";
+        this.volume = 10.0;
+        this.period = "30";
+        this.price = 5000;
+    }
+    void standard(int memberNum){
+        this.memberNum = memberNum;
+        this.rating = "standard";
+        this.volume = 20.0;
+        this.period = "30";
+        this.price = 10000;
+    }
+    void premium(int memberNum){
+        this.memberNum = memberNum;
+        this.rating = "premium";
+        this.volume = 30.0;
+        this.period = "30";
+        this.price = 15000;
+    }
+}
